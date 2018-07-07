b2ab4442a5e186b984cd8842175b9d8f747bfd67
kundan-whitehat-kk <Kundan_kk52@outlook.com>
2018-07-07 19:47:13 +0530

    implementing notification..

---
+++ app/src/main/java/tbc/techbytecare/kk/androiddrinksodaproject/Helper/NotificationHelper.java
@@ -0,0 +1,55 @@
+package tbc.techbytecare.kk.androiddrinksodaproject.Helper;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.net.Uri;
+import android.os.Build;
+
+import tbc.techbytecare.kk.androiddrinksodaproject.R;
+
+public class NotificationHelper extends ContextWrapper {
+
+    public static final String CHANNEL_ID = "tbc.techbytecare.kk.androiddrinksodaproject.TBC";
+    public static final String CHANNEL_NAME = "Drink Shop";
+
+    private NotificationManager manager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT);
+        channel.enableLights(false);
+        channel.enableVibration(true);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if (manager == null)    {
+            manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return manager;
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public Notification.Builder getDrinkShopNotification(String title, String message, Uri soundUri)    {
+        return new Notification.Builder(getApplicationContext(),CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSound(soundUri)
+                .setAutoCancel(true);
+    }
+}
--- app/src/main/java/tbc/techbytecare/kk/androiddrinksodaproject/Serivces/MyFirebaseMessaging.java
+++ app/src/main/java/tbc/techbytecare/kk/androiddrinksodaproject/Serivces/MyFirebaseMessaging.java
@@ -1,12 +1,70 @@
 package tbc.techbytecare.kk.androiddrinksodaproject.Serivces;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.Random;
+
+import tbc.techbytecare.kk.androiddrinksodaproject.Helper.NotificationHelper;
+import tbc.techbytecare.kk.androiddrinksodaproject.R;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getData() != null)    {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                sendNotificationAPI26(remoteMessage);
+            }
+            else    {
+                sendNotification(remoteMessage);
+            }
+        }
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Map<String,String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String message = data.get("message");
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(new Random().nextInt(),builder.build());
+    }
+
+    private void sendNotificationAPI26(RemoteMessage remoteMessage) {
+        Map<String,String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String message = data.get("message");
+
+        NotificationHelper helper;
+        Notification.Builder builder;
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        helper = new NotificationHelper(this);
+        builder = helper.getDrinkShopNotification(title,message,defaultSoundUri);
+
+        helper.getManager().notify(new Random().nextInt(),builder.build());
     }
 }
